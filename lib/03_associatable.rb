181479824ff686ea74070a044f2262978ca6524e
Peter Zheng <mmk606@gmail.com>
2015-10-16 19:32:39 -0400

    Finish belongs_to and has_many.

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -20,7 +20,7 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    @class_name = options[:class_name] || name.capitalize
+    @class_name = options[:class_name] || name.singularize.capitalize
     @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
     @primary_key = options[:primary_key] || :id
   end
@@ -37,17 +37,24 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
+    options = BelongsToOptions.new(name.to_s, options)
     define_method(name) do
-      puts self.to_s
-      fk = self.send(:id)
-      self.class.send(model_class)
-      self.class.send(where(primary_key: fk)).first
+      foreign_key = send(options.foreign_key)
+      params = {(options.primary_key)=> foreign_key}
+      options.model_class.where(params).first
     end
+
   end
 
   def has_many(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name.to_s, options)
+    p options
+    define_method(name) do
+      primary_key = send(options.primary_key)
+      params = {(options.foreign_key)=> primary_key}
+      p params
+      options.model_class.where(params)
+    end
   end
 
   def assoc_options
